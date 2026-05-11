aaced7edd86907a7f948626a953f44fecfe603ae
icjhomz <36946766+icjhomz@users.noreply.github.com>
2026-05-11 22:20:46 +0800

    Fix: GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jmmtouristtransportservices/',
+  base: '/jmm_tourist_transport_services/',
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
